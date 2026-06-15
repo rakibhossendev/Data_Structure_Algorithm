db1d84d402eaa4ab801e982d2c011081a169d986
rakibhossendev <rh01828315879@gmail.com>
2026-06-15 14:17:19 +0600

    add solution

--- Patterns/test.cpp
+++ Patterns/test.cpp
@@ -4,14 +4,15 @@ int main(){
     int n =4;
 
     for(int i=0; i<n; i++){
-        for(int j =0; j<n-i; j++){
+        for(int j =0; j<n-i-1; j++){
             cout << " ";
         }
+
         for(int j =0; j<i+1; j++){
-            cout << "* ";
+            cout << "*";
         }
+
         cout << endl;
     }
-
     return 0;
 }
